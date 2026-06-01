3be30560b051cd64076a5a3f28740c6eb8f2f976
Ambady Unnikrishnan <your-email@example.com>
2026-06-01 11:37:10 +0530

    Fixings the CORS isuues and refresh Issues in Agent Side

--- mini-lottos-agent/backend/server.js
+++ mini-lottos-agent/backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://mini-lottos-app.vercel.app', 'https://mini-lottos-web-app-agent.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
